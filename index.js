7f4004785de0f4427dc686ffed3ab9171023bd56
jatinhemnani01 <jatinhemnani01@gmail.com>
2021-09-20 15:28:47 +0530

    added INSER in db

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-const app = require("express")();
-const mysql = require('mysql')
+const express = require("express");
+const mysql = require("mysql");
 
+
+const app = express();
+
+app.use(express.json());
 
 const db = mysql.createConnection({
-    host:'localhost',
-    port:9000,
-    user:'root',
-    password:'1234',
-    database:'test'
-})
-
-db.connect((err)=>{
-    if(err) throw err;
-    console.log("mysql Connected")
-})
-
-
-app.get("/", (_, res) => {
-    db.query('SELECT * FROM `User`',(err,result)=>{
-	if(err){
-	    console.log(err)
-	}
-	res.send(result)
-    })
+  host: "localhost",
+  port: 9000,
+  user: "root",
+  password: "1234",
+  database: "test",
 });
 
+//DB Connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("mysql Connected");
+});
 
+// Root Route
+app.get("/", (_, res) => {
+  db.query("SELECT * FROM `User`", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
+});
 
+app.post("/add", (req, res) => {
+  const {name,age,city,Email}=req.body;
+  const post = {
+    name: name,
+    age: age,
+    city: city,
+    Email: Email,
+  };
 
+  db.query("INSERT INTO User SET ?", post, (err, result) => {
+    if (err) {
+      res.json({ message: "Error" });
+    }
+
+    res.json(post);
+  });
+});
+
+// SERVER PORT
 app.listen(5000, () => {
   console.log("Server Running");
 });
